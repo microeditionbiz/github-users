17e4134e8f4153918bfdfa56cda59395e779038b
Pablo Romero <romero7116@gmail.com>
2022-08-07 18:08:59 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 07/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/GitHub_UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_UsersApp.swift
+//  Shared
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
